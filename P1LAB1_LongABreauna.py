7b74da6a1a55604fb1d893b67d29ca50beee723d
abreaunal <kichelle7@icloud.com>
2026-04-05 20:13:15 -0400

    Add files via upload

---
+++ P1LAB1_LongABreauna.py
@@ -0,0 +1,15 @@
+# A'Breauna Long
+ # 4/5/2026
+ # P1LAB1(Inout and Output Basics)
+ # This program asks the user for their first and last name and prints a welcome message. 
+
+# Ask the user for their first name 
+first_name = input("Enter your first name: ")
+
+# Ask the user for their last name 
+last_name = input("Enter your last name: ")
+
+# Display a welcome message 
+print("Hello, ", first_name, last_name, "!", "Welcome to cti-110.")
+
+
